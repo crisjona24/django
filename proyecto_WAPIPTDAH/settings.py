18f228568ac9d0c553051f44988b07c6a04de1e4
Cristobal Jonathan Rios Simancas <78310047+crisjona24@users.noreply.github.com>
2023-12-29 16:12:04 +0000

    listo

--- proyecto_WAPIPTDAH/settings.py
+++ proyecto_WAPIPTDAH/settings.py
@@ -36,7 +36,7 @@ DEBUG = True
 # ACCESS CONTROL ALLOW ORIGIN
 
 
-ALLOWED_HOSTS = ['*'] 
+#ALLOWED_HOSTS = ['*'] 
  
  #ALLOWED_HOSTS = ['proyectofinaldjango.herokuapp.com','mi-primer-app.azurewebsites.net']
 
@@ -232,11 +232,12 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = (
+CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
     "https://react-frontend-production-b574.up.railway.app",
-)
-
+    # Agrega más dominios permitidos aquí si es necesario
+]
 
 # DOMINIO   
 #SITE_URL = "http://aprender-wapiptdah.com"

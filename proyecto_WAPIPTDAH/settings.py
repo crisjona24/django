c9dd436143b19217fffca825a27c0ce483f77a6e
Cristobal Jonathan Rios Simancas <78310047+crisjona24@users.noreply.github.com>
2023-12-29 17:06:11 +0000

    listo

--- proyecto_WAPIPTDAH/settings.py
+++ proyecto_WAPIPTDAH/settings.py
@@ -105,13 +105,7 @@ DATABASES = {
 
 """ CONFIGURACIÓN DE BASE DE DATOS POSTGRESQL """
 
-"""
-DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
-}
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
